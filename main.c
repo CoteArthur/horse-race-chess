e2fa9db33e2ebc8167b8ef730d0c0aa7a0c2459f
CoteArthur <57949230+CoteArthur@users.noreply.github.com>
2020-10-20 15:51:27 +0200

    better looping / threading handling

--- main.c
+++ main.c
@@ -5,8 +5,10 @@
 #include <math.h>
 #include <sys/wait.h>
 
+#define NBPLAYERS 4
 #define PIPES_LENGHT 24
 #define INT_SIZE sizeof(int)
+#define PLAYERS_SIZE NBPLAYERS*INT_SIZE
 
 void closePipes(int *);
 int play (int, int);
@@ -16,85 +18,94 @@ int main() {
     int pipes[PIPES_LENGHT],
 		players[] = {0, 0, 0, 0},
 		curPlaying = 0,
-		token = 0,
 		i;
 
 	for (i=0; i<PIPES_LENGHT; i+=2) pipe(pipes+i);
 
-	while (1) {
-		//Player 0
-		if ((bufferPid = fork()) == 0) {
+	//Player 0
+	if ((bufferPid = fork()) == 0) {
+		while (1) {
+			read(pipes[8], &curPlaying, INT_SIZE);
+			if (curPlaying == -1) break;
 			if (curPlaying == 0) {
 				players[0] = play(players[0], 0);
-				token++;
-				write(pipes[1], &token, INT_SIZE);
-				read(pipes[6], &token, INT_SIZE);
-				if (!token) exit(-1);
+				write(pipes[1], players, PLAYERS_SIZE);
+				read(pipes[6], players, PLAYERS_SIZE);
 				write(pipes[11], players, INT_SIZE);
 			} else {
-				read(pipes[6], &token, INT_SIZE);
-				write(pipes[1], &token, INT_SIZE);
+				read(pipes[6], players, PLAYERS_SIZE);
+				write(pipes[1], players, PLAYERS_SIZE);
 			}
-			closePipes(pipes);
-			exit(0);
-		} else if (bufferPid == -1) exit(-1);
-		
-		//Player 1
-		if ((bufferPid = fork()) == 0) {
+		}
+		closePipes(pipes);
+		exit(0);
+	} else if (bufferPid == -1) exit(-1);
+	
+	//Player 1
+	if ((bufferPid = fork()) == 0) {
+		while (1) {
+			read(pipes[12], &curPlaying, INT_SIZE);
+			if (curPlaying == -1) break;
 			if (curPlaying == 1) {
 				players[1] = play(players[1], 1);
-				token++;
-				write(pipes[3], &token, INT_SIZE);
-				read(pipes[0], &token, INT_SIZE);
-				if (!token) exit(-1);
+				write(pipes[3], players, PLAYERS_SIZE);
+				read(pipes[0], players, PLAYERS_SIZE);
 				write(pipes[15], players+1, INT_SIZE);
 			} else {
-				read(pipes[0], &token, INT_SIZE);
-				write(pipes[3], &token, INT_SIZE);
+				read(pipes[0], players, PLAYERS_SIZE);
+				write(pipes[3], players, PLAYERS_SIZE);
 			}
-			closePipes(pipes);
-			exit(0);
-		} else if (bufferPid == -1) exit(-1);
+		}
+		closePipes(pipes);
+		exit(0);
+	} else if (bufferPid == -1) exit(-1);
 
-		//Player 2
-		if ((bufferPid = fork()) == 0) {
+	//Player 2
+	if ((bufferPid = fork()) == 0) {
+		while (1) {
+			read(pipes[16], &curPlaying, INT_SIZE);
+			if (curPlaying == -1) break;
 			if (curPlaying == 2) {
 				players[2] = play(players[2], 2);
-				token++;
-				write(pipes[5], &token, INT_SIZE);
-				read(pipes[2], &token, INT_SIZE);
-				if (!token) exit(-1);
+				write(pipes[5], players, PLAYERS_SIZE);
+				read(pipes[2], players, PLAYERS_SIZE);
 				write(pipes[19], players+2, INT_SIZE);
 			} else {
-				read(pipes[2], &token, INT_SIZE);
-				write(pipes[5], &token, INT_SIZE);
+				read(pipes[2], players, PLAYERS_SIZE);
+				write(pipes[5], players, PLAYERS_SIZE);
 			}
-			closePipes(pipes);
-			exit(0);
-		} else if (bufferPid == -1) exit(-1);
+		}
+		closePipes(pipes);
+		exit(0);
+	} else if (bufferPid == -1) exit(-1);
 
-		//Player 3
-		if ((bufferPid = fork()) == 0) {
+	//Player 3
+	if ((bufferPid = fork()) == 0) {
+		while (1) {
+			read(pipes[20], &curPlaying, INT_SIZE);
+			if (curPlaying == -1) break;
 			if (curPlaying == 3) {
 				players[3] = play(players[3], 3);
-				token++;
-				write(pipes[7], &token, INT_SIZE);
-				read(pipes[4], &token, INT_SIZE);
-				if (!token) exit(-1);
+				write(pipes[7], players, PLAYERS_SIZE);
+				read(pipes[4], players, PLAYERS_SIZE);
 				write(pipes[23], players+3, INT_SIZE);
 			} else {
-				read(pipes[4], &token, INT_SIZE);
-				write(pipes[7], &token, INT_SIZE);
+				read(pipes[4], players, PLAYERS_SIZE);
+				write(pipes[7], players, PLAYERS_SIZE);
 			}
-			closePipes(pipes);
-			exit(0);
-		} else if (bufferPid == -1) exit(-1);
+		}
+		closePipes(pipes);
+		exit(0);
+	} else if (bufferPid == -1) exit(-1);
 
-		//Parent
+	//Parent
+	while (1) {
+		for (i=9; i<=21; i+=4) write(pipes[i], &curPlaying, INT_SIZE);
+
 		switch (curPlaying)
 		{
 			case 0:
-				read(pipes[10], players+0, INT_SIZE);
+				read(pipes[10], players, INT_SIZE);
 				break;
 			case 1:
 				read(pipes[14], players+1, INT_SIZE);
@@ -113,16 +124,18 @@ int main() {
 
 		if (players[curPlaying] == 62) {
 			printf("Player %d won\n", curPlaying);
+			curPlaying = -1;
+			for (i=9; i<=21; i+=4) write(pipes[i], &curPlaying, INT_SIZE);
 			break;
 		}
 
 		curPlaying == 3 ? curPlaying = 0 : curPlaying++;
 
-		while (wait(NULL)!=-1);
 		usleep(500000);
 	}
 	
 	closePipes(pipes);
+	while (wait(NULL)!=-1);
 	exit(0);
 }
 
